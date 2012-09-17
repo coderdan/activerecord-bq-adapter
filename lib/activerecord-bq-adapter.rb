9533c319b36413cf03973095a6973affe2fce18b
Dan Draper <daniel@codefire.com>
2012-09-18 09:24:40 +0930

    first commit

---
+++ lib/activerecord-bq-adapter.rb
@@ -0,0 +1,9 @@
+require "activerecord-bq-adapter/version"
+
+module Activerecord
+  module Bq
+    module Adapter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activerecord-bq-adapter/version.rb
@@ -0,0 +1,7 @@
+module Activerecord
+  module Bq
+    module Adapter
+      VERSION = "0.0.1"
+    end
+  end
+end
